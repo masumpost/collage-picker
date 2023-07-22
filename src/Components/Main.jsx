3b2ef4b1bdef3aeb510b6fb5eae2a4d1cc74dcae
md-masum <masum.nextu@gmail.com>
2023-07-22 16:30:51 +0600

    created footer

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Home/Home/Header/Navbar";
+import Footer from "./Footer";
 
 const Main = () => {
     return (
         <div className="mx-2 md:mx-12">
          <Navbar></Navbar>
-         <Outlet></Outlet>   
+         <Outlet></Outlet>
+         <Footer></Footer>  
         </div>
     );
 };
